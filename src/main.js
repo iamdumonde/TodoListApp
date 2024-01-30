3c5af3914ad841409bdbadf4dbc7a1875c56d8b2
iamdumonde <martinaboumonde@gmail.com>
2024-01-30 19:56:35 +0100

    la navbar

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import "./style.css"
 import App from './App.vue'
 import router from './router' 
 import { pinia } from './stores';
-import { useTodoStore } from './stores';
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia);
 app.mount('#app')
-// app.provide(useTodoStore)
 
